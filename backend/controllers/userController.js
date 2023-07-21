89aae5a08814354bf2987f30f3e2713666afe0ff
Shaktidhar <sdmohapatra7@gmail.com>
2023-07-21 17:27:54 +0530

    create user login with JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,10 @@
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncError = require('../middleware/catchAsyncError');
 const User = require('../models/userModel');
+const sendToken = require('../utils/jwtToken');
 
 
-//Register a user
+//Register A User
 exports.registerUser = catchAsyncError(async(req,res,next)=>{
     const {name,email,password} = req.body;
     const user = await User.create({
@@ -14,8 +15,26 @@ exports.registerUser = catchAsyncError(async(req,res,next)=>{
         }
     });
 
-    res.status(201).json({
-        success:true,
-        user
-    });
+    sendToken(user,201,res);  // call the token function here
 });
+
+//Login User
+exports.loginUser = catchAsyncError(async(req,res,next)=>{
+    const {email,password} = req.body;
+    //check if user has given password and email both
+    if(!email || !password){
+        return next(new ErrorHandler("Please Enter Email and Password",400));
+    }
+
+    const user = await User.findOne({email}).select("+password");
+    if(!user){
+        return next(new ErrorHandler("Invalid Email or Password",401));
+    }
+
+    const isPasswordMatch = await user.comparePassword(password);
+    if(!isPasswordMatch){
+        return next(new ErrorHandler("Invalid Email or Password",401));
+    }
+
+    sendToken(user,200,res);
+})
