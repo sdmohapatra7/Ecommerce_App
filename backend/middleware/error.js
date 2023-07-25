901e072226eedd131707f80e405b54385e385268
Shaktidhar <sdmohapatra7@gmail.com>
2023-07-25 18:32:07 +0530

    handle mongoose duplicate key err,wronge jwt err and jwt expaire err

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,24 @@ module.exports = (err,req,res,next)=>{
         err = new ErrorHandler(message,400);
     }
 
+    //mongoose duplicate key error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message,400);
+    }
+
+    //Wrong JWT Error
+    if(err.name === 'JsonWebTokenError'){
+        const message = `Json Web Token Is Invalid, try again`;
+        err = new ErrorHandler(message,400);
+    }
+
+    //JWT Expaire Error
+    if(err.name === 'TokenExpairedError'){
+        const message = `Json Web Token is Expaired, try again`;
+        err = new ErrorHandler(message,400);
+    }
+
     res.status(err.statusCode).json({
         success:false,
         error:err.message,
