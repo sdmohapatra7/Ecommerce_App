7fa5c85302d421e1967910431e9669a9c2a80031
Shaktidhar <sdmohapatra7@gmail.com>
2023-07-26 17:11:14 +0530

    Create or update review

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -15,7 +15,7 @@ const productSchema = new mongoose.Schema({
         required:[true,"Please Enter product price"],
         maxLength:[8,"price can't exceed 8 character"]
     },
-    rating:{
+    ratings:{
         type:Number,
         default:0
     },
@@ -47,6 +47,11 @@ const productSchema = new mongoose.Schema({
     },
     reviews:[
         {
+            user:{
+                type:mongoose.Schema.ObjectId,
+                ref:'User',
+                required: true
+            },
             name:{
                 type:String,
                 required:true
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController');
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview } = require('../controllers/productController');
 const {isAuthenticatedUser, authorizeRoles} = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+
 router.route('/products').get(getAllProducts);
+
 router.route('/product/:id').get(getProductDetails);
+
 router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
+
 router.route('/admin/product/:id').delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router.route('/review').put(isAuthenticatedUser, createProductReview);
 
 module.exports = router;
