dd21e049dd2960a705fc15a4ac4e4a492caf4738
Shaktidhar <sdmohapatra7@gmail.com>
2023-08-19 10:25:45 +0530

    show the state data in home page

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,52 +1,54 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import Product from './Product.js';
 import MetaData from '../layout/MetaData';
 
-import {getProduct} from '../../actions/productAction';
-import {useSelector,useDispatch} from 'react-redux';
+import { getProduct } from '../../actions/productAction';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 
-const product = {
-    name: 'Red TShirt',
-    images:[{url:'https://i.ibb.co/DRST11n/1.webp'}],
-    price: '3000',
-    _id:'shakti'
-}
+// const product = {
+//     name: 'Red TShirt',
+//     images:[{url:'https://i.ibb.co/DRST11n/1.webp'}],
+//     price: '3000',
+//     _id:'shakti'
+// }
 export default function Home() {
     const dispatch = useDispatch();
+    const { loading, error, products, productsCount } = useSelector(
+        (state) => state.products
+    );
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getProduct());
-    },[dispatch]);
+    }, [dispatch]);
     return (
         <>
-            <MetaData title='Ecommerce'/>
-            <div className="banner">
-                <p>Welcome to Ecommerce</p>
-                <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            {loading ? (
+                'loading'
+            ) : (
+                <>
+                    <MetaData title='Ecommerce' />
+                    <div className="banner">
+                        <p>Welcome to Ecommerce</p>
+                        <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-                <a href="#container">
-                    <button>
-                        Scroll <CgMouse />
-                    </button>
-                </a>
-            </div>
+                        <a href="#container">
+                            <button>
+                                Scroll <CgMouse />
+                            </button>
+                        </a>
+                    </div>
 
-            <h2 className="homeHeading">Featured Products</h2>
+                    <h2 className="homeHeading">Featured Products</h2>
 
-            <div className="container" id="container">
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            </div>
+                    <div className="container" id="container">
+                        {products && products.map((product, index) => <Product product={product} key={index} />)}
+                    </div>
+                </>
+            )}
         </>
     )
 }
--- frontend/src/components/Home/Product.js
+++ frontend/src/components/Home/Product.js
@@ -2,25 +2,26 @@ import React from 'react';
 import ReactStar from 'react-rating-stars-component';
 import { Link } from 'react-router-dom';
 
-const options = {
+
+
+export default function Product({product}) {
+  const options = {
     edit:false,
     color:'rgba(20,20,20,0.1)',
     activeColor:'tomato',
     // size:window.innerWidth<600 ?20:23,
-    value:2.5,
+    value:product.ratings,
     isHalf:true
 }
-
-export default function Product({product}) {
   return (
     <Link className='productCard' to={product._id}>
         <img src={product.images[0].url} alt={product.name}/>
         <p>{product.name}</p>
       <div>
         <ReactStar {...options} />
-        <span className="productCardSpan">(264 Reviews)</span>
+        <span className="productCardSpan">({product.numOfReviews} Reviews)</span>
       </div>
-      <span>{product.price}</span>
+      <span>{`${product.price}`}</span>
     </Link>
   )
 }
