13b1137e21d9754641bb6113a676b22f7c505fb9
Shaktidhar Mohapatra <sdmohapatra7@gmail.com>
2025-08-27 00:13:32 +0530

    css changes

--- frontend/src/components/layout/Header/Header.js
+++ frontend/src/components/layout/Header/Header.js
@@ -1,47 +1,77 @@
-import React from "react";
-import { ReactNavbar } from "overlay-navbar";
-import { MdAccountCircle, MdSearch, MdAddShoppingCart } from "react-icons/md";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  MdAccountCircle,
+  MdSearch,
+  MdAddShoppingCart,
+} from "react-icons/md";
 import logo from "../../images/logo.png";
-
-const options = {
-  logo,
-  burgerColorHover: "#28c74f",
-  logoWidth: "20vmax",
-  navColor1: "white",
-  logoHoverSize: "10px",
-  logoHoverColor: "#28c74f",
-  link1Text: "Home",
-  link2Text: "Products",
-  link3Text: "Contact",
-  link4Text: "About",
-  link1Url: "/",
-  link2Url: "/products",
-  link3Url: "/contact",
-  link4Url: "/about",
-  link1Size: "1.3vmax",
-  link1Color: "rgba(35, 35, 35,0.8)",
-  link1ColorHover: "#28c74f",
-  link1Margin: "1vmax",
-  nav1justifyContent: "flex-end",
-  nav2justifyContent: "flex-end",
-  nav3justifyContent: "flex-start",
-  nav4justifyContent: "flex-start",
-  profileIcon: true,
-  ProfileIconElement: MdAccountCircle,
-  profileIconUrl: "/login",
-  profileIconColor: "rgba(35, 35, 35,0.8)",
-  profileIconColorHover: "#28c74f",
-  searchIcon: true,
-  SearchIconElement: MdSearch,
-  searchIconColor: "rgba(35, 35, 35,0.8)",
-  searchIconColorHover: "#28c74f",
-  cartIcon: true,
-  CartIconElement: MdAddShoppingCart,
-  cartIconColor: "rgba(35, 35, 35,0.8)",
-  cartIconColorHover: "#28c74f",
-  cartIconMargin: "1vmax",
-};
+import "./Header.css";
 
 export default function Header() {
-  return <ReactNavbar {...options} />;
+  const [showUserMenu, setShowUserMenu] = useState(false);
+  const [showProductMenu, setShowProductMenu] = useState(false);
+
+  return (
+    <header className="customNavbar">
+      {/* Logo */}
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+      </div>
+
+      {/* Main Navigation */}
+      <nav className="navLinks">
+        <Link to="/">Home</Link>
+
+        {/* Product Dropdown */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => setShowProductMenu(true)}
+          onMouseLeave={() => setShowProductMenu(false)}
+        >
+          <span>Products ▾</span>
+          {showProductMenu && (
+            <div className="dropdown-menu">
+              <Link to="/products">All Products</Link>
+              <Link to="/admin/product">Add Product</Link>
+              <Link to="/admin/products">Manage Products</Link>
+            </div>
+          )}
+        </div>
+
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      {/* Right Side Icons */}
+      <div className="navIcons">
+        <Link to="/search">
+          <MdSearch />
+        </Link>
+        <Link to="/cart">
+          <MdAddShoppingCart />
+        </Link>
+
+        {/* User Dropdown */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => setShowUserMenu(true)}
+          onMouseLeave={() => setShowUserMenu(false)}
+        >
+          <MdAccountCircle className="icon" />
+          {showUserMenu && (
+            <div className="dropdown-menu user-menu">
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/account">Profile</Link>
+              <Link to="/orders">My Orders</Link>
+              <Link to="/password/forgot">Forgot Password</Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 }
